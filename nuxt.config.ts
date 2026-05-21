d20a28656fd8960a39a24897f128bba6e2b5cbd4
FreddyCro <freddy10100@hotmail.com>
2026-05-21 10:16:21 +0800

    wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,61 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+
+  // 讓區網設備可以使用，例如手機
+  // devServer: {
+  //   host: "0.0.0.0",
+  //   port: 3000,
+  // },
+
+  ssr: true,
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      failOnError: false,
+    },
+  },
+
+  runtimeConfig: {
+    public: {
+      APP_MODE: "",
+      APP_ASSETS_PATH: "",
+    },
+  },
+
+  app: {
+    baseURL: (() => {
+      const nuxtUrl = process.env.NUXT_URL;
+      if (!nuxtUrl) return "/";
+      try {
+        return new URL(nuxtUrl).pathname;
+      } catch {
+        return "/";
+      }
+    })(),
+  },
+
   vite: {
     optimizeDeps: {
-      include: ['earcut', 'deck.gl', '@deck.gl/core', '@deck.gl/layers'],
+      include: ["earcut", "deck.gl", "@deck.gl/core", "@deck.gl/layers"],
     },
     plugins: [
       {
-        name: 'earcut-cjs-to-esm',
-        apply: 'serve',
+        name: "earcut-cjs-to-esm",
+        apply: "serve",
         transform(code: string, id: string) {
           if (/[/\\]earcut[/\\]src[/\\]earcut\.js/.test(id)) {
-            const esm = code
-              .replace("'use strict';", '')
-              .replace('module.exports = earcut;', '')
-              .replace('module.exports.default = earcut;', '')
-              + '\nexport default earcut;\n';
+            const esm =
+              code
+                .replace("'use strict';", "")
+                .replace("module.exports = earcut;", "")
+                .replace("module.exports.default = earcut;", "") +
+              "\nexport default earcut;\n";
             return { code: esm, map: null };
           }
         },
       },
     ],
   },
-})
+});
